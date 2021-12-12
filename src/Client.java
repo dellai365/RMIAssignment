1cc749128fe35e032d0dc4b2f125d858a460750a
dellai365 <77741324+dellai365@users.noreply.github.com>
2021-12-12 19:35:02 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,17 @@
 import java.rmi.Naming;
+import java.util.*;
 
 public class Client {
 
     public static void main(String[] args) {
         try {
-           MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+                MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+                Scanner obj = new Scanner(System.in);
+                String s = obj.nextLine();
+
+                System.out.println(service.caseChanger(s));
+
+                obj.close();
 
         } catch (Exception e) {
             e.printStackTrace();
