08320d335f9a6f64fbd8cfcb972bacbd98eac9d2
dellai365 <77741324+dellai365@users.noreply.github.com>
2021-12-12 19:42:08 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -7,11 +7,45 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
     public Server() throws RemoteException {}
 
+    public String reverse(String s) {
+        Character ch; 
+        String ans="";
+        for (int i=0; i<s.length(); i++)
+        {
+            ch= s.charAt(i); 
+            ans=ch+ans; 
+        }
+             return ans ;
+    }
+
+    public Character minChar(String s){
+        char min = 'z';
+
+        for (int i=0; i<s.length()-1; i++)   
+            if (s.charAt(i) < min)
+                min = s.charAt(i);  
+
+                return min;         
+    }
+
+    public String caseChanger(String s){
+
+           StringBufferans = new StringBuffer(s); 
+
+         for (int i=0; i<s.length(); i++) {
+                if (Character.isLowerCase(s.charAt(i)))
+                     ans.setCharAt(i, Character.toUpperCase(s.charAt(i)));
+                else   ans.setCharAt(i, Character.toLowerCase(s.charAt(i)));
+         }
+
+        return ans.toString();
+    }
+
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         Server server = new Server();
 
-        java.rmi.registry.LocateRegistry.createRegistry(1250);
+java.rmi.registry.LocateRegistry.createRegistry(1250);
 
-        Naming.rebind("rmi://127.0.0.1:1250/server", server);
+Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
 }
